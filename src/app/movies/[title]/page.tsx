9f55086c46f4dfb91b9801e4854cf30fa73d85c4
Lutfian Rhdn <lutfian.rhdn@gmail.com>
2023-07-08 12:50:53 +0700

    bugfix:responsive :sparkles:

--- src/app/movies/[title]/page.tsx
+++ src/app/movies/[title]/page.tsx
@@ -14,14 +14,14 @@ export default async function MoviePage({ params }: any) {
   const data = await getData()
   return (
     <>
-      <div className="flex justify-center items-center h-screen ">
+      <div className="flex justify-center items-center md:h-screen ">
 
-        <div className="flex md:grid-cols-2 max-h-96  h-full ">
+        <div className="flex flex-col md:flex-row md:grid-cols-2 max-h-96  h-full ">
           <div className="max-h-96 md:h-screen">
-            <img className="h-full object-top" src={data.poster_url} alt="" />
+            <img className="h-full w-full" src={data.poster_url} alt="" />
           </div>
           <div className="flex bg-white  md:p-10 px-5 py-2 h-full">
-            <div className="mb-auto mt-auto max-w-lg">
+            <div className="mb-auto mt-auto max-w-lg pb-10">
               <h1 className="md:text-3xl text-xl font-bold uppercase">{data.title}</h1>
               <p className="font-semibold text-md md:mb-5">Rp.{data.ticket_price}</p>
               <p className="font-semibold mb-5">Age Rating: {data.age_rating}</p>
